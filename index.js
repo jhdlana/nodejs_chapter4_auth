d3134bb1bb0863d304a06c90a68c15404fcb5ae8
jhdlana <ljh06031@gmail.com>
2024-05-04 23:41:05 +0900

    express server로 로그인 구현

--- index.js
+++ index.js
@@ -5,18 +5,17 @@ const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 
-
 const secretText = 'secretText'
 const refreshSecretText = 'refreshSecretText'
 
 const app = express()
 
 app.use(cors({
-    origin : 'http://127.0.0.1:5500',
+    origin : 'http://127.0.0.1:5501',
     methods: 'POST, GET'
 }))
 
-let refreshToken = [];
+let refreshTokens = [];
 app.use(cookieParser())
 app.use(express.json())
 
@@ -26,6 +25,16 @@ app.post('/login', (req, res) => {
     console.log(`${req.method} 요청이 들어옴`)
 
     const accessToken = jwt.sign(user, secretText, {expiresIn: '30s'})
+
+    const refreshToken = jwt.sign(user, refreshSecretText, { expiresIn: '1d'})
+        refreshTokens.push(refreshToken)
+
+        //refreshToken을 쿠키에 넣어주기
+        res.cookie('jwt', refreshToken, {
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000,
+    })
+
 
     res.json({accessToken : accessToken})
 })
@@ -49,6 +58,26 @@ function authMiddleware(req, res, next) {
     })
 }
 
+app.get('/refresh', (req, res) => {
+    // cookies 가져오기 cookie-parser
+    const cookies = req.cookies
+    if(!cookies?.jwt) return res.sendStatus(403)
+
+    const refreshToken = cookies.jwt
+    // refreshToken이 데이터베이스에 있는 토큰인지 확인
+    if(!refreshTokens.includes(refreshToken)){
+        return res.sendStatus(403)
+    }
+
+    // token이 유효한 토큰인지 확인
+    jwt.verify(refreshToken, refreshSecretText, (err, user) => {
+        if(err) return res.sendStatus(403)
+        // accessToken을 생성하기
+        const accessToken = jwt.sign({name: user.name}, secretText, {expiresIn: '30s'})
+
+        res.json({accessToken})
+    })
+})
 
 // 서버가 4000번 포트에서 듣기를 시작합니다. 서버가 시작되면 콘솔에 메시지를 출력합니다.
 const port = 4000;
